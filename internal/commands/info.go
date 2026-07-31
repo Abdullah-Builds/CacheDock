8614fb0482e5fe2d608fe9df5f85e07631d637b6
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2026-08-01 04:50:42 +0600

    fix: safely read atomic stats in INFO command

--- internal/commands/info.go
+++ internal/commands/info.go
@@ -8,16 +8,16 @@ import (
 )
 
 func Info(parts []string, cacheServer *cache.Cache, conn net.Conn) (string, bool) {
-	
+
 	stats := cacheServer.Stats()
 
 	msg := fmt.Sprintf(
 		"Requests=%d Hits=%d Misses=%d Sets=%d Deletes=%d",
-		stats.Requests,
-		stats.Hits,
-		stats.Misses,
-		stats.Sets,
-		stats.Deletes,
+		stats.Requests.Load(),
+		stats.Hits.Load(),
+		stats.Misses.Load(),
+		stats.Sets.Load(),
+		stats.Deletes.Load(),
 	)
 
 	return msg, true
